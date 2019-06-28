04f06f76992e08dea6e8a5e16748b42d9971fcb2
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-06-28 09:08:27 +0000

    build based on 51d51cd

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#GalerkinEtena.jl-1","page":"GalerkinEtena.jl","title":"GalerkinEtena.jl","text":"","category":"section"},{"location":"#","page":"GalerkinEtena.jl","title":"GalerkinEtena.jl","text":"Documentation for GalerkinEtena.jl","category":"page"}]
+[{"location":"#GalerkinEtena.jl-1","page":"Documentation","title":"GalerkinEtena.jl","text":"","category":"section"},{"location":"#","page":"Documentation","title":"Documentation","text":"Documentation for GalerkinEtena.jl","category":"page"}]
 }
