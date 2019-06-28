2b996284aad55c1c738f5da35ae52a1778eee7d3
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-06-28 08:49:53 +0000

    build based on 986ac55

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#GalerkinEtena.jl-1","page":"GalerkinEtena.jl","title":"GalerkinEtena.jl","text":"","category":"section"},{"location":"#","page":"GalerkinEtena.jl","title":"GalerkinEtena.jl","text":"Documentation for GalerkinEtena.jl","category":"page"}]
+}
