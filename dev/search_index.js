f936796e58f49390464bcf13b9e6135fde725365
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-06-28 09:19:00 +0000

    build based on 08df69f

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#GalerkinEtena.jl-1","page":"Documentation","title":"GalerkinEtena.jl","text":"","category":"section"},{"location":"#","page":"Documentation","title":"Documentation","text":"Documentation for GalerkinEtena.jl","category":"page"}]
+[{"location":"#GalerkinEtena.jl-1","page":"Documentation","title":"GalerkinEtena.jl","text":"","category":"section"},{"location":"#","page":"Documentation","title":"Documentation","text":"Documentation for GalerkinEtena.jl","category":"page"},{"location":"#","page":"Documentation","title":"Documentation","text":"Modules = [LyonCalcul]\nOrder   = [:type, :function]","category":"page"}]
 }
