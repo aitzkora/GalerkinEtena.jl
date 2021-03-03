773d344b29171a95e1152ead1f27824402d1de06
aitzkora <marc.fuentes@inria.fr>
2021-03-03 15:14:29 +0100

    add a parameter type for simplex meshes :
    - now we must adapt 1D code to make it compatible

--- src/mesh.jl
+++ src/mesh.jl
@@ -8,3 +8,16 @@ struct Mesh1D
        new(pts, c)
    end
 end
+
+
+struct SimplexMesh{D} 
+  dim::Int64
+  points::Array{Float64,2}
+  cells::Array{Int64,2}
+  function SimplexMesh{D}(points::Array{Float64,2}, cells::Array{Int64,2}) where {D}
+    @assert Val(D) isa Union{map(x->Val{x},1:3)...}
+    @assert D == size(points, 2)
+    @assert D+1 == size(cells, 2)
+    new{D}(D, points, cells)
+  end
+end
